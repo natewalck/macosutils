e52c9fb6b0038d50eb16f864849268926d134058
Nate Walck <nate.walck@gmail.com>
2019-01-27 16:50:50 -0500

    Add formatter

--- installer.go
+++ installer.go
@@ -8,20 +8,22 @@ import (
 	"path"
 )
 
+// InstallApp installs a .app to /Applications from the specified path.
 func InstallApp(appPath string, appName string) error {
 	args := []string{"--noqtn", path.Join(appPath, appName), path.Join("/Applications/", appName)}
 	_, err := exec.Command("/usr/bin/ditto", args...).Output()
 	if err != nil {
-		log.Printf("Failed to install:", appName)
+		log.Printf("Failed to install: %v", appName)
 	}
 	return err
 }
 
+// InstallPkg runs installed on the specified pkg path.
 func InstallPkg(pkgPath string, pkgName string) error {
 	args := []string{"-pkg", path.Join(pkgPath, pkgName), "-tgt", "/"}
 	_, err := exec.Command("/usr/sbin/installer", args...).Output()
 	if err != nil {
-		log.Printf("Failed to install:", pkgName)
+		log.Printf("Failed to install: %v", pkgName)
 	}
 	return err
 }
